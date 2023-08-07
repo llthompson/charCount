afb944510547719589a68a05e98f736b6ed436db
llthompson <lisalacythompson+github@gmail.com>
2023-08-07 16:01:40 -0500

    done

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-let input = 'h3!!  oOO';
+let input = 'The quick brown fox jumps over 18 lazy dogs.';
 
 const charCount = (input) => {
     const justLetters = input.toLowerCase().replace(/[^a-z]/g, '');
 
+    console.log('Just the letters: ', justLetters)
+
+    const nowSort = justLetters.split('').sort().join('');
+
+    console.log('Sort them: ', nowSort);
+
     const theCount = {};
 
-    for (let letter of justLetters) {
+    for (let letter of nowSort) {
         if (theCount[letter]) {
             theCount[letter]++;
         } else {
